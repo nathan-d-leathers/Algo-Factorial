e112cb5605f11544cd6e7808768b688805731848
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-25 13:06:51 -0700

    solved via python

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,16 @@
-exports.factorial = function(num) {
+exports.factorial = (num) => {
+    let answer = 1;
+    if (num < 2) {
+        return num;
+    }
+
+
+    for (let i = num; i>= 2; i--) {
+        answer = answer * i;
+    }
+
+    return answer;
+
 };
+
+// console.log(exports.factorial(8))
